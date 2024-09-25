ec72f441ef8b5a205ef1a9a801225208f81a7114
shibaan1 <askarishibaan1@gmail.com>
2024-09-25 00:23:21 -0700

    Add files via upload

---
+++ LAB Q5 PART1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int size = 6;
+	int array[size];
+	  
+	  
+	  for(int i=0; i<size; i++)
+	  {
+	  cout<<"enter the elements of array :" ;
+	  cin>>array[i];
+       }
+	cout<<"the array will be ";
+	for(int i=0; i<size; i++){
+		cout<<array[i]<<" ";
+	}
+	return 0;
+	
+	
+	
+	
+	
+	
+}
