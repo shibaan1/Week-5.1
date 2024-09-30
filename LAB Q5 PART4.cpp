98a56683aa3c48ae20d67b7e552ef4d9f1fc5a6d
shibaan1 <askarishibaan1@gmail.com>
2024-09-30 09:58:19 +0530

    Update LAB Q5 PART4.cpp

--- LAB Q5 PART4.cpp
+++ LAB Q5 PART4.cpp
@@ -1,13 +1,10 @@
 #include<iostream>
 using namespace std;
-
 int main()
-
 {
 int array[10]={2,4,58,8,46,85,56,19,47,25};
 int max = array[0];
 int secondMax=INT_MIN;
-
 for(int i=1; i<10; i++){
 	if(array[i] > max) {
 		secondMax = max;
@@ -17,16 +14,6 @@ for(int i=1; i<10; i++){
 		secondMax = array[i];
 	}
 }
-
 cout<<"second largest number in array is"<<secondMax<<endl;
 return 0 ;
-
-
-
-
-
-
-
-
-
 }
