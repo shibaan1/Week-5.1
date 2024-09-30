dd7512dfa01df6bceeaaa15833323ac45615ee62
shibaan1 <askarishibaan1@gmail.com>
2024-09-30 09:56:29 +0530

    Update LAB Q5 PART3.cpp

--- LAB Q5 PART3.cpp
+++ LAB Q5 PART3.cpp
@@ -2,14 +2,8 @@
 using namespace std;
 int main()
 {
-	
-	int array[]={6,67,7,2,5,7,4,8,4,4,66,555,7,766};
-	
-	int sizeofarray = sizeof(array)/sizeof(int
-	
-	0);
-	
+	int array[]={6,67,7,2,5,7,4,8,4,4,66,555,7,766};	
+	int sizeofarray = sizeof(array)/sizeof(int);
 	cout<<"size of array is:"<<sizeofarray;
-	
 	return 0;
 }
