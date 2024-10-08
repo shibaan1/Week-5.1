7d33ad6d0332b3c050c65cbf9b7503151f9d96e7
shibaan1 <askarishibaan1@gmail.com>
2024-10-08 09:41:52 +0530

    Delete LAB Q5 PART3.cpp

--- LAB Q5 PART3.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	int array[]={6,67,7,2,5,7,4,8,4,4,66,555,7,766};	
-	int sizeofarray = sizeof(array)/sizeof(int);
-	cout<<"size of array is:"<<sizeofarray;
-	return 0;
-}
